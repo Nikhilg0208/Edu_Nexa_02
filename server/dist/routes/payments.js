1c6bec07eda925bd28f136bf225400e1c10f9f42
Nikhilg0208 <nikhilg1098@gmail.com>
2024-11-27 00:03:09 +0530

    Remove dist folder from repository and ignore it

--- server/dist/routes/payments.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var payments_js_1 = require("../controllers/payments.js");
-var paymentRoutes = express_1.default.Router();
-paymentRoutes.post("/capturePayment", payments_js_1.capturePayment);
-// router.post("/verifySignature", verifySignature)
-exports.default = paymentRoutes;
--- server/dist/test/Auth.test.js
+++
@@ -1,4 +0,0 @@
-// const { sum } = require("../controllers/Auth");
-// test("adds 1 + 2 to equal 3", () => {
-//   expect(sum(3, 2)).toBe(5);
-// });
