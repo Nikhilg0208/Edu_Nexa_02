3f3c6f886417eaba18fe67c5a1b187e5061db570
Nikhilg0208 <nikhilg1098@gmail.com>
2025-03-03 19:32:22 +0530

    chage in login page

--- portal/src/pages/Login.jsx
+++ portal/src/pages/Login.jsx
@@ -44,13 +44,11 @@ const Login = () => {
           dispatch(setAuth({ token }));
           navigate("/admin/dashboard");
         }
-      } else {
-        const error = res.error;
-        const message = error.dat.message;
-        toast.error(message);
       }
     } catch (error) {
-      console.error("Error during login:", error);
+      setEmail("");
+      setPassword("");
+      toast.error("Your Email is not registered.");
     }
   };
 
@@ -98,7 +96,7 @@ const Login = () => {
                 type={showPassword ? "text" : "password"}
                 autoComplete="current-password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
                 placeholder="Password"
                 value={password}
                 onChange={handlePasswordChange}
